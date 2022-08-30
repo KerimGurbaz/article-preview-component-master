ebf816bbc198b5c2439d5d1b202b5acd4f0b22e0
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-31 01:02:45 +0200

    js added

---
+++ app.js
@@ -0,0 +1,11 @@
+const arrow = document.querySelector(".arrow");
+let share = document.querySelector(".top-share")
+
+
+
+arrow.addEventListener("click", (e) =>{
+    share.style.display="block"
+   
+})
+ 
+
